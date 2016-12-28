e6abbc871261c4ab9a640f162a73535a97e40a12
pdziekan <pdziekanp@gmail.com>
2016-12-28 17:16:39 +0100

    various updates

--- coal_fluctu.cpp
+++ coal_fluctu.cpp
@@ -32,6 +32,12 @@ using namespace libcloudphxx::lgrngn;
     n2_stp = float(40e6) / si::cubic_metres;
 
 
+//globals
+std::array<float, 51> rad_bins;
+int n_cell;
+float rho_stp_f;
+
+
 
 // lognormal aerosol distribution
 template <typename T>
@@ -62,10 +68,59 @@ void two_step(particles_proto_t<float> *prtcls,
 }
 
 
+void diag(particles_proto_t<float> *prtcls, std::array<float, 51> &res_bins)
+{
+  prtcls->diag_sd_conc();
+  std::cout << "sd conc: " << prtcls->outbuf()[0] << std::endl;
+
+/*
+  prtcls->diag_all();
+  prtcls->diag_dry_mom(0);
+  drop_no = prtcls->outbuf()[0];
+  std::cout << "number of droplets: " << drop_no << std::endl;
+
+  prtcls->diag_all();
+  prtcls->diag_wet_mom(1);
+  std::cout << "mean wet radius: " << prtcls->outbuf()[0] / drop_no << std::endl;
+
+  prtcls->diag_all();
+  prtcls->diag_dry_mom(1);
+  std::cout << "mean dry radius: " << prtcls->outbuf()[0] / drop_no << std::endl;
+
+  prtcls->diag_all();
+  prtcls->diag_wet_mom(3);
+  std::cout << "3rd wet mom: " << prtcls->outbuf()[0] << std::endl;
+*/
+  // get mass density function
+  for (int i=0; i <rad_bins.size() -1; ++i)
+  {
+//    prtcls->diag_all();
+  //  prtcls->diag_wet_size_spectr( rad_bins[i], 0.62 ); //sigma0 = 0.62 like in Shima (2009)
+    prtcls->diag_wet_rng(rad_bins[i], rad_bins[i+1]);
+    prtcls->diag_wet_mom(0);
+    float rad = (rad_bins[i] + rad_bins[i+1]) / 2.;
+    float mean = 0;
+    auto buf = prtcls->outbuf();
+    for(int c=0; c < n_cell; ++c)
+    {
+//      std::cout << buf[c] << " ";
+      mean += buf[c];
+    }
+    mean /= n_cell;
+    
+    res_bins[i]= mean * rho_stp_f / 1e6 // now its number per cm^3
+                     * 3.14 *4. / 3. *rad * rad * rad * 1e3 * 1e3;  // to get mass in grams
+                     // / (rad_bins[i+1] - rad_bins[i]); // to get density function
+//    of_size_spectr << rad * 1e6 << " " << res_bins_pre[i] << " " << res_bins_post[i] << std::endl; 
+
+  }
+}
+
+
+
 int main(){
-printf("start\n");
-//  std::ofstream of_mass_dens("mass_dens.dat");
-printf("1\n");
+  std::ofstream of_size_spectr("size_spectr.dat");
+  std::ofstream of_max_rw_std_dev("max_rw_std_dev.dat");
 
   opts_init_t<float> opts_init;
 
@@ -76,7 +131,7 @@ printf("1\n");
   opts_init.sstp_cond = 1; 
   opts_init.kernel = kernel_t::hall;
   opts_init.terminal_velocity = vt_t::beard77fast;
-  opts_init.dx = 1e-2;
+  opts_init.dx = 10000e-2;
   opts_init.dy = 1e-2;
   opts_init.dz = 1e-2; 
 
@@ -88,7 +143,7 @@ printf("1\n");
 //  opts_init.dy = 1;
 //  opts_init.dz = 1; 
 
-  const int nx = 1e7;
+  const int nx = 1e0;
   const int ny = 1;
   const int nz = 1;
 
@@ -100,13 +155,12 @@ printf("1\n");
   opts_init.z1 = opts_init.nz * opts_init.dz;
   opts_init.rng_seed = time(NULL);
 
-  int n_cell = opts_init.nx * opts_init.ny * opts_init.nz;
+  n_cell = opts_init.nx * opts_init.ny * opts_init.nz;
 
 //  opts_init.sd_conc = 100;
   opts_init.sd_const_multi = 1;
-  opts_init.n_sd_max = 30 * opts_init.nx * opts_init.ny * opts_init.nz;
+  opts_init.n_sd_max = 30e6 * opts_init.x1 * opts_init.y1 * opts_init.z1 + 1;
 
-  std::array<float, 51> rad_bins;
   std::array<float, 51> res_bins_pre;
   std::array<float, 51> res_bins_post;
   std::iota(rad_bins.begin(), rad_bins.end(), 0);
@@ -130,16 +184,16 @@ printf("1\n");
   ); 
 */
 
-  opts_init.dry_sizes[0.] = {{17e-6, 20}, {21.4e-6, 10}};
+  opts_init.dry_sizes[0.] = {{17e-6, 20e6}, {21.4e-6, 10e6}};
 
   particles_proto_t<float> *prtcls;
      prtcls = factory<float>(
-        (backend_t)multi_CUDA, 
+        (backend_t)OpenMP, 
         opts_init
       );
 
   using libcloudphxx::common::earth::rho_stp;
-  const float rho_stp_f = (rho_stp<float>() / si::kilograms * si::cubic_metres);
+  rho_stp_f = (rho_stp<float>() / si::kilograms * si::cubic_metres);
   std::cout << "rho stp f = " << rho_stp_f << std::endl;
 
 //  std::array<float, nx*ny*nz> pth;
@@ -172,100 +226,40 @@ printf("1\n");
   opts.coal = 1;
   opts.rcyc = 0;
 
-  prtcls->diag_sd_conc();
-  std::cout << "sd conc: " << prtcls->outbuf()[0] << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_dry_mom(0);
-  auto drop_no = prtcls->outbuf()[0];
-  std::cout << "number of droplets: " << drop_no << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_wet_mom(1);
-  std::cout << "mean wet radius: " << prtcls->outbuf()[0] / drop_no << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_dry_mom(1);
-  std::cout << "mean dry radius: " << prtcls->outbuf()[0] / drop_no << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_wet_mom(3);
-  std::cout << "3rd wet mom: " << prtcls->outbuf()[0] << std::endl;
-
-  // get mass density function
-  for (int i=0; i <rad_bins.size() -1; ++i)
-  {
-//    prtcls->diag_all();
-  //  prtcls->diag_wet_mass_dens( rad_bins[i], 0.62 ); //sigma0 = 0.62 like in Shima (2009)
-    prtcls->diag_wet_rng(rad_bins[i], rad_bins[i+1]);
-    prtcls->diag_wet_mom(0);
-    float rad = (rad_bins[i] + rad_bins[i+1]) / 2.;
-    float mean = 0;
-    auto buf = prtcls->outbuf();
-//    std::cout << "radius (" << rad_bins[i] << ", " << rad_bins[i+1] << "): ";
-    for(int c=0; c < n_cell; ++c)
-    {
-//      std::cout << buf[c] << " ";
-      mean += buf[c];
-    }
-//    std::cout << std::endl;
-    mean /= n_cell;
-    
-    res_bins_pre[i]= mean * rho_stp_f / 1e6 // now its number per cm^3
-                     * 3.14 *4. / 3. *rad * rad * rad * 1e3 * 1e3;  // to get mass in grams
-                     // / (rad_bins[i+1] - rad_bins[i]); // to get density function
-  }
+  diag(prtcls, res_bins_pre);
 
 //  prtcls->step_sync(opts,th,rv);//,rhod);
 //  cout << prtcls->step_async(opts) << endl;
 
   for(int i=0; i<sim_time; ++i)
+  {
     two_step(prtcls,th,rv,opts);
+    // get std dev of max rw
+    prtcls->diag_max_rw();
+    auto arr = prtcls->outbuf();
+    float mean = 0;
+    for(int j=0; j<n_cell; ++j)
+    {
+      mean += std::pow(arr[j], 3); // mass std dev...
+    }
+    mean /= float(n_cell);
+    float std_dev = 0;
+    for(int j=0; j<n_cell; ++j)
+      std_dev += std::pow(std::pow(arr[j], 3) / mean - 1, 2);
+    std_dev = std::sqrt(std_dev / n_cell);
+
+    of_max_rw_std_dev << i * opts_init.dt << " " << std_dev << std::endl;
+  }
 
   std::cout << "po symulacji:" << std::endl;
 
-  prtcls->diag_sd_conc();
-  std::cout << "sd conc: " << prtcls->outbuf()[0] << std::endl;
+  diag(prtcls, res_bins_post);
 
-  prtcls->diag_all();
-  prtcls->diag_dry_mom(0);
-  drop_no = prtcls->outbuf()[0];
-  std::cout << "number of droplets: " << drop_no << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_wet_mom(1);
-  std::cout << "mean wet radius: " << prtcls->outbuf()[0] / drop_no << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_dry_mom(1);
-  std::cout << "mean dry radius: " << prtcls->outbuf()[0] / drop_no << std::endl;
-
-  prtcls->diag_all();
-  prtcls->diag_wet_mom(3);
-  std::cout << "3rd wet mom: " << prtcls->outbuf()[0] << std::endl;
-
-  // get mass density function
+// output
   for (int i=0; i <rad_bins.size() -1; ++i)
   {
-//    prtcls->diag_all();
-  //  prtcls->diag_wet_mass_dens( rad_bins[i], 0.62 ); //sigma0 = 0.62 like in Shima (2009)
-    prtcls->diag_wet_rng(rad_bins[i], rad_bins[i+1]);
-    prtcls->diag_wet_mom(0);
     float rad = (rad_bins[i] + rad_bins[i+1]) / 2.;
-    float mean = 0;
-    auto buf = prtcls->outbuf();
-    for(int c=0; c < n_cell; ++c)
-    {
-//      std::cout << buf[c] << " ";
-      mean += buf[c];
-    }
-    mean /= n_cell;
-    
-    res_bins_post[i]= mean * rho_stp_f / 1e6 // now its number per cm^3
-                     * 3.14 *4. / 3. *rad * rad * rad * 1e3 * 1e3;  // to get mass in grams
-                     // / (rad_bins[i+1] - rad_bins[i]); // to get density function
-//    of_mass_dens << rad * 1e6 << " " << res_bins_pre[i] << " " << res_bins_post[i] << std::endl; 
-
+    of_size_spectr << rad * 1e6 << " " << res_bins_pre[i] << " " << res_bins_post[i] << std::endl; 
   }
 
 
